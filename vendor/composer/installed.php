cb5d918445a581283873b6ab24d5142a0d809abf
Gleb <glebkarmanov@MacBook-Air-Gleb.local>
2024-07-11 16:35:39 +0200

    Игры разума. Задание 9 - исправления

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hexlet/code',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bd2b814a2409eebaa4b1d56ecdf137700ab0468e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hexlet/code' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bd2b814a2409eebaa4b1d56ecdf137700ab0468e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.10.1',
+            'version' => '3.10.1.0',
+            'reference' => '8f90f7a53ce271935282967f53d0894f8f1ff877',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.11.22',
+            'version' => '0.11.22.0',
+            'reference' => 'a6bb94664ca36d0962f9c2ff25591c315a550c51',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
