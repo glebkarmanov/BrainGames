62fa322a34ca13adba932addc149d95c44d28f7f
Gleb <glebkarmanov@MacBook-Air-Gleb.local>
2024-07-17 15:29:45 +0400

    Исправления задание 9 - Поправил текст в Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -10,7 +10,7 @@ use function cli\prompt;
 
 function greetUser($gameType)
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     if ($gameType === 'calc') {
